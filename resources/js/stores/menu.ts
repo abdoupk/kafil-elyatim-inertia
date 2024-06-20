8d2a6d1dad73ec58758dd76c633d497057b21987
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-06-20 04:22:12 +0100

    families create side

--- resources/js/stores/menu.ts
+++ resources/js/stores/menu.ts
@@ -1,8 +1,9 @@
 import { __ } from '@/utils/i18n'
 import { defineStore } from 'pinia'
+import type { IFormattedMenu } from '@/types/types'
 
 export const useMenuStore = defineStore('menu', {
-    state: () => ({
+    state: (): { menu: Array<IFormattedMenu | 'divider'> } => ({
         menu: [
             {
                 icon: 'icon-house',
@@ -37,9 +38,47 @@ export const useMenuStore = defineStore('menu', {
                 ]
             },
             {
-                icon: 'icon-family',
+                icon: 'icon-branches',
                 routeName: '',
                 title: __('branches'),
+                subMenu: [
+                    {
+                        icon: 'icon-code-merge',
+                        title: __('list', { attribute: __('branches') }),
+                        routeName: 'tenant.branches.index',
+                        url: route('tenant.branches.index')
+                    },
+                    {
+                        icon: 'icon-grid-round-2-plus',
+                        title: __('add new family'),
+                        routeName: 'tenant.branches.create',
+                        url: route('tenant.branches.create')
+                    }
+                ]
+            },
+            {
+                icon: 'icon-users-gear',
+                routeName: '',
+                title: __('members'),
+                subMenu: [
+                    {
+                        icon: 'icon-users',
+                        title: __('list', { attribute: __('branches') }),
+                        routeName: 'tenant.branches.index',
+                        url: route('tenant.branches.index')
+                    },
+                    {
+                        icon: 'icon-user-plus',
+                        title: __('add new family'),
+                        routeName: 'tenant.branches.create',
+                        url: route('tenant.branches.create')
+                    }
+                ]
+            },
+            {
+                icon: 'icon-children',
+                routeName: '',
+                title: __('orphans'),
                 subMenu: [
                     {
                         icon: 'icon-branches',
@@ -50,8 +89,71 @@ export const useMenuStore = defineStore('menu', {
                     {
                         icon: 'icon-users-plus',
                         title: __('add new family'),
-                        routeName: 'tenant.families.create',
-                        url: route('tenant.families.create')
+                        routeName: 'tenant.branches.create',
+                        url: route('tenant.branches.create')
+                    }
+                ]
+            },
+            {
+                icon: 'icon-gear',
+                routeName: 'tenant.settings.index',
+                title: __('settings'),
+                url: route('tenant.families.index')
+            },
+            {
+                icon: 'icon-chart-pie-simple',
+                routeName: '',
+                title: __('statistics'),
+                subMenu: [
+                    {
+                        icon: 'icon-branches',
+                        title: __('list', { attribute: __('branches') }),
+                        routeName: 'tenant.branches.index',
+                        url: route('tenant.branches.index')
+                    },
+                    {
+                        icon: 'icon-users-plus',
+                        title: __('add new family'),
+                        routeName: 'tenant.branches.create',
+                        url: route('tenant.branches.create')
+                    }
+                ]
+            },
+            {
+                icon: 'icon-calendar-star',
+                routeName: '',
+                title: __('occasions'),
+                subMenu: [
+                    {
+                        icon: 'icon-branches',
+                        title: __('list', { attribute: __('branches') }),
+                        routeName: 'tenant.branches.index',
+                        url: route('tenant.branches.index')
+                    },
+                    {
+                        icon: 'icon-users-plus',
+                        title: __('add new family'),
+                        routeName: 'tenant.branches.create',
+                        url: route('tenant.branches.create')
+                    }
+                ]
+            },
+            {
+                icon: 'icon-hands-holding-dollar',
+                routeName: '',
+                title: __('financial'),
+                subMenu: [
+                    {
+                        icon: 'icon-branches',
+                        title: __('list', { attribute: __('branches') }),
+                        routeName: 'tenant.branches.index',
+                        url: route('tenant.branches.index')
+                    },
+                    {
+                        icon: 'icon-users-plus',
+                        title: __('add new family'),
+                        routeName: 'tenant.branches.create',
+                        url: route('tenant.branches.create')
                     }
                 ]
             }
