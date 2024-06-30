0c78a1f2fe537d8f17faa82191297140360a9292
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-06-30 18:03:40 +0100

    the search desktop structuring

--- resources/js/Components/top-bar/search/TheSearchDesktopLayout.vue
+++ resources/js/Components/top-bar/search/TheSearchDesktopLayout.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
-import { ComboboxInput, Combobox as HeadlessCombobox, TransitionRoot } from '@headlessui/vue'
+import { TransitionRoot } from '@headlessui/vue'
+import type { Hit } from 'meilisearch'
 import { twMerge } from 'tailwind-merge'
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 
 import BaseFormInput from '@/Components/Base/form/BaseFormInput.vue'
 import SvgLoader from '@/Components/SvgLoader.vue'
-import TheNoResultsFound from '@/Components/top-bar/search/TheNoResultsFound.vue'
-import TheSearchResults from '@/Components/top-bar/search/TheSearchResults.vue'
 
+import { getResultsSize } from '@/utils/helper'
+import { search } from '@/utils/search'
 import { useComputedAttrs } from '@/utils/useComputedAttrs'
 
 defineOptions({
@@ -44,21 +45,65 @@ function onKeydown(event: KeyboardEvent) {
     }
 }
 
+const results = ref<Hit[]>([])
+
+const resultsRefs = ref<HTMLElement[]>([])
+
+const selectedIndex = ref(0)
+watch(
+    () => query.value,
+    async (query: string) => {
+        if (query != '') await search(query).then((res) => (results.value = res.map((r) => r.hits)))
+    },
+    { immediate: true }
+)
+
+const goTo = (step: string) => {
+    console.log(step)
+    if (results.value[selectedIndex.value]) {
+        window.location = results.value[selectedIndex.value].url
+    }
+}
+const onTermKeydown = (event) => {
+    console.log(event.code)
+    if (['ArrowUp', 'ArrowDown'].includes(event.code)) {
+        event.preventDefault()
+    }
+    console.log(selectedIndex.value)
+    switch (event.code) {
+        case 'ArrowDown':
+            if (selectedIndex.value === getResultsSize(results.value) - 1) {
+                selectedIndex.value = 0
+            } else {
+                selectedIndex.value += 1
+            }
+            break
+        case 'ArrowUp':
+            if (selectedIndex.value === 0) {
+                selectedIndex.value = getResultsSize(results.value) - 1
+            } else {
+                selectedIndex.value -= 1
+            }
+            break
+    }
+
+    console.log(getResultsSize(results.value))
+    console.log(selectedIndex.value, '0000')
+
+    resultsRefs.value[selectedIndex.value]?.scrollIntoView(false)
+}
+
 onMounted(() => {
     window.addEventListener('keydown', onKeydown)
 })
-
-onUnmounted(() => {
-    window.removeEventListener('keydown', onKeydown)
-})
+onUnmounted(() => window.removeEventListener('keydown', onKeydown))
 </script>
 
 <template>
-    <headless-combobox v-slot="{ activeIndex }" as="div" class="relative hidden sm:block">
-        <combobox-input
-            v-model="query"
+    <div class="relative hidden sm:block">
+        <base-form-input
             id="search_dropdown"
-            :as="BaseFormInput"
+            v-model="query"
             :class="
                 twMerge(
                     'w-56 rounded-full border-transparent bg-slate-200 pe-8 shadow-none transition-[width] duration-300 ease-in-out focus:w-72 focus:border-transparent dark:bg-darkmode-400',
@@ -69,10 +114,11 @@ onUnmounted(() => {
             type="text"
             v-bind="attrs.attrs"
             @blur="searchDropdown = false"
-            @change="query = $event.target.value"
             @focus="searchDropdown = true"
+            @input="query = ($event.target as HTMLInputElement).value"
             @keydown.esc.prevent="closeSearch"
-        ></combobox-input>
+            @keydown="onTermKeydown"
+        ></base-form-input>
         <svg-loader
             class="absolute inset-y-0 end-0 my-auto me-3 h-5 w-5 text-slate-600 dark:text-slate-500 rtl:rotate-90"
             name="icon-search"
@@ -88,13 +134,56 @@ onUnmounted(() => {
             leave-from="mt-[3px] visible opacity-100 translate-y-0"
             leave-to="mt-5 invisible opacity-0 translate-y-1"
         >
-            <div class="absolute end-0 z-10 mt-[3px]">
-                <the-search-results @close="closeSearch" :activeIndex :query class="max-h-[500px] w-[450px]">
-                    <template #notFound>
-                        <the-no-results-found class="box w-[450px] text-center"></the-no-results-found>
-                    </template>
-                </the-search-results>
+            <div class="absolute end-0 z-10 mt-[3px] box max-h-96 overflow-y-auto px-5 pt-5">
+                <div v-for="(result, key, index) in results" :key="`${key}_${index}`">
+                    <div
+                        v-if="result[0]?.index"
+                        :class="{ 'mb-0': Object.keys(results).length - 1 === index }"
+                        class="mb-5"
+                    >
+                        <div class="mb-2 font-medium ltr:capitalize">
+                            <!-- TODO: change breadcrumb -->
+                            {{ $t(`breadcrumb.${result[0]?.index}`) }}
+                        </div>
+
+                        <div
+                            v-for="(info, i_index) in result"
+                            :key="`${info.title}_${i_index}`"
+                            :ref="
+                                (el) => {
+                                    resultsRefs[key * result.length + i_index] = el
+                                }
+                            "
+                            :class="{
+                                '-ms-1 rounded-md bg-slate-200 ps-1 dark:bg-darkmode-300':
+                                    selectedIndex === key * result.length + i_index
+                            }"
+                            @mousemove="selectedIndex = key * result.length + i_index"
+                        >
+                            <a
+                                class="-my-2 mt-2.5 flex items-center py-1 pe-1"
+                                href="javascript:void(0)"
+                                @click.prevent="goTo(info.link)"
+                            >
+                                <div
+                                    v-if="info.icon"
+                                    :class="info.icon.color"
+                                    class="flex h-8 w-8 items-center justify-center rounded-full"
+                                >
+                                    <svg-loader :name="info.icon.icon" class="w-h h-4"></svg-loader>
+                                </div>
+                                <div v-else class="image-fit h-8 w-8">
+                                    <img :alt="info.title" :src="info.image" class="rounded-full" />
+                                </div>
+                                <div class="ms-3 ltr:capitalize">{{ info.title }}</div>
+                                <div v-if="info.hint" class="ms-auto w-48 truncate text-end text-xs text-slate-500">
+                                    {{ info.hint }}
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </transition-root>
-    </headless-combobox>
+    </div>
 </template>
