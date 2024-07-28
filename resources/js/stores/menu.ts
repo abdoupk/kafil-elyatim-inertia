c6c81f04050f15aa5db272224c83229d67f78001
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-07-28 16:41:44 +0100

    fix some issues in menu

--- resources/js/stores/menu.ts
+++ resources/js/stores/menu.ts
@@ -10,27 +10,27 @@ export const useMenuStore = defineStore('menu', {
             {
                 icon: 'icon-house',
                 routeName: 'tenant.dashboard',
-                url: route('tenant.dashboard'),
+                url: '/dashboard',
                 title: __('dashboard')
             },
             'divider',
             {
                 icon: 'icon-user-lock',
                 routeName: 'tenant.roles.index',
-                url: route('tenant.roles.index'),
+                url: '/dashboard/roles',
                 title: __('roles')
             },
             {
                 icon: 'icon-users-gear',
                 title: __('the_members'),
                 routeName: 'tenant.members.index',
-                url: route('tenant.members.index')
+                url: '/dashboard/members'
             },
             {
                 icon: 'icon-branches',
                 title: __('branches'),
                 routeName: 'tenant.branches.index',
-                url: route('tenant.branches.index')
+                url: '/dashboard/branches'
             },
             'divider',
             {
@@ -42,13 +42,13 @@ export const useMenuStore = defineStore('menu', {
                         icon: 'icon-users',
                         title: __('list', { attribute: __('the_families') }),
                         routeName: 'tenant.families.index',
-                        url: route('tenant.families.index')
+                        url:'/dashboard/families'
                     },
                     {
                         icon: 'icon-users-plus',
                         title: n__(__('add new'), 0, { attribute: __('family') }),
                         routeName: 'tenant.families.create',
-                        url: route('tenant.families.create')
+                        url: '/dashboard/families/create'
                     }
                 ]
             },
@@ -56,13 +56,13 @@ export const useMenuStore = defineStore('menu', {
                 icon: 'icon-children',
                 title: __('orphans'),
                 routeName: 'tenant.orphans.index',
-                url: route('tenant.orphans.index')
+                url: '/dashboard/orphans'
             },
             {
                 icon: 'icon-hands-holding-child',
                 title: __('the_sponsors'),
                 routeName: 'tenant.sponsors.index',
-                url: route('tenant.sponsors.index')
+                url: '/dashboard/sponsors'
             },
             {
                 icon: 'icon-calendar-star',
@@ -73,7 +73,7 @@ export const useMenuStore = defineStore('menu', {
                         icon: 'icon-ram',
                         title: __('eid_el_adha'),
                         routeName: 'tenant.occasions.eid-al-adha.index',
-                        url: route('tenant.occasions.eid-al-adha.index')
+                        url:'/dashboard/occasions/eid-al-adha'
                     },
                     {
                         icon: 'icon-backpack',
@@ -85,13 +85,13 @@ export const useMenuStore = defineStore('menu', {
                         icon: 'icon-moon-stars',
                         title: __('eid_el_fitr'),
                         routeName: 'tenant.occasions.eid-suit.index',
-                        url: route('tenant.occasions.eid-suit.index')
+                        url: '/dashboard/occasions/eid-suit'
                     },
                     {
                         icon: 'icon-moon',
                         title: __('ramadan basket'),
                         routeName: 'tenant.occasions.ramadan-basket.index',
-                        url: route('tenant.occasions.ramadan-basket.index')
+                        url: '/dashboard/occasions/ramadan-basket'
                     },
                     {
                         icon: 'icon-basket-shopping',
@@ -106,49 +106,49 @@ export const useMenuStore = defineStore('menu', {
                 icon: 'icon-chart-pie-simple',
                 title: __('statistics'),
                 routeName: 'tenant.statistics.index',
-                url: route('tenant.statistics.index')
+                url: '/dashboard/statistics'
             },
             {
                 icon: 'icon-map-location-dot',
                 title: __('the_zones'),
                 routeName: 'tenant.zones.index',
-                url: route('tenant.zones.index')
+                url: '/dashboard/zones'
             },
             {
                 icon: 'icon-hands-holding-dollar',
                 title: __('financial'),
                 routeName: 'tenant.financial.index',
-                url: route('tenant.financial.index')
+                url: '/dashboard/financial'
             },
             {
                 icon: 'icon-shelves',
                 title: __('the_inventory'),
                 routeName: 'tenant.inventory.index',
-                url: route('tenant.inventory.index')
+                url: '/dashboard/inventory'
             },
             {
                 icon: 'icon-handshake-angle',
                 title: __('the_needs'),
                 routeName: 'tenant.needs.index',
-                url: route('tenant.needs.index')
+                url: '/dashboard/needs'
             },
             {
                 icon: 'icon-gear',
                 routeName: 'tenant.settings.index',
                 title: __('settings'),
-                url: route('tenant.settings.index')
+                url: '/dashboard/settings'
             },
             {
                 icon: 'icon-graduation-cap',
                 routeName: 'tenant.lessons.index',
                 title: __('private_lessons'),
-                url: route('tenant.lessons.index')
+                url:'/dashboard/lessons'
             },
             {
                 icon: 'icon-school-lock',
                 routeName: 'tenant.schools.index',
                 title: __('private_schools'),
-                url: route('tenant.schools.index')
+                url: '/dashboard/schools'
             }
         ]
     })
