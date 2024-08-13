9bc7167004afd4f3b7329736f70b1be050f91066
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-13 11:57:21 +0100

    Optimize Trash

--- app/Http/Controllers/V1/Trash/TrashIndexController.php
+++ app/Http/Controllers/V1/Trash/TrashIndexController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\V1\Trash;
 
 use App\Http\Controllers\Controller;
+use DB;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -10,6 +13,70 @@ class TrashIndexController extends Controller
 {
     public function __invoke(): Response
     {
-        return Inertia::render('Tenant/trash/TrashIndexPage');
+        $perPage = (int) request()->input('perPage', 10);
+        $currentPage = Paginator::resolveCurrentPage();
+        $startIndex = ($currentPage - 1) * $perPage;
+
+        $unionQuery = "
+    (
+        SELECT
+            f.id,
+            f.name,
+            f.deleted_at,
+            u.id AS user_id,
+            CONCAT(u.first_name, ' ', u.last_name) AS user_name,
+            'families' AS type
+        FROM families f
+        LEFT JOIN users u ON f.deleted_by = u.id
+        WHERE f.deleted_at IS NOT NULL AND f.tenant_id = :tenant_id
+    )
+    UNION ALL
+    (
+        SELECT
+            o.id,
+            o.first_name AS name,
+            o.deleted_at,
+            u.id AS user_id,
+            CONCAT(u.first_name, ' ', u.last_name) AS user_name,
+            'orphans' AS type
+        FROM orphans o
+        LEFT JOIN users u ON o.deleted_by = u.id
+        WHERE o.deleted_at IS NOT NULL AND o.tenant_id = :tenant_id
+    )
+    UNION ALL
+    (
+        SELECT
+            s.id,
+            s.first_name AS name,
+            s.deleted_at,
+            u.id AS user_id,
+            CONCAT(u.first_name, ' ', u.last_name) AS user_name,
+            'sponsors' AS type
+        FROM sponsors s
+        LEFT JOIN users u ON s.deleted_by = u.id
+        WHERE s.deleted_at IS NOT NULL AND s.tenant_id = :tenant_id
+    )";
+        $items = DB::table(DB::raw("($unionQuery) AS temp_table"))
+            ->offset($startIndex)
+            ->limit($perPage)
+            ->orderByDesc('deleted_at')
+            ->setBindings(['tenant_id' => tenant('id')])
+            ->get();
+
+        $totalItems = DB::table(DB::raw("($unionQuery) AS temp_table"))
+            ->orderByDesc('deleted_at')
+            ->setBindings(['tenant_id' => tenant('id')])
+            ->count();
+
+        return Inertia::render('Tenant/trash/TrashIndexPage', [
+            'items' => new LengthAwarePaginator(
+                $items,
+                $totalItems,
+                $perPage,
+                $currentPage,
+                ['path' => Paginator::resolveCurrentPath()]
+            ),
+            'params' => getParams(),
+        ]);
     }
 }
--- resources/js/Pages/Tenant/trash/TrashIndexPage.vue
+++ resources/js/Pages/Tenant/trash/TrashIndexPage.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
+import type { PaginationData } from '@/types/types'
+
 import TheLayout from '@/Layouts/TheLayout.vue'
 
 defineOptions({
     layout: TheLayout
 })
+
+const props = defineProps<{
+    items: PaginationData<unknown>
+}>()
+
+console.log(props.items)
 </script>
 
 <template>trash</template>
