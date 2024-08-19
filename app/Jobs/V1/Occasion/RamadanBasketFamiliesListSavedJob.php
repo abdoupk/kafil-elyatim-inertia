a2d9c4fa294c2994d2066f06e5f3b5aadd20eecc
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-19 03:25:28 +0100

    Structuring Jobs

---
+++ app/Jobs/V1/Occasion/RamadanBasketFamiliesListSavedJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs\V1\Occasion;
+
+use App\Models\Branch;
+use App\Models\User;
+use App\Notifications\Branch\CreateBranchNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Notification;
+
+class RamadanBasketFamiliesListSavedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(public Branch $branch, public User $user) {}
+
+    public function handle(): void
+    {
+        Notification::send(
+            User::whereHas('settings', function ($query) {
+                return $query->where('notifications->branches_and_zones_changes', true);
+            })->where('users.id', '!=', $this->user->id)->get(),
+            new CreateBranchNotification(branch: $this->branch, user: $this->user));
+    }
+}
---
+++ app/Jobs/V1/Zone/ZoneCreatedJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs\V1\Zone;
+
+use App\Models\Branch;
+use App\Models\User;
+use App\Notifications\Branch\CreateBranchNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Notification;
+
+class ZoneCreatedJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(public Branch $branch, public User $user) {}
+
+    public function handle(): void
+    {
+        Notification::send(
+            User::whereHas('settings', function ($query) {
+                return $query->where('notifications->branches_and_zones_changes', true);
+            })->where('users.id', '!=', $this->user->id)->get(),
+            new CreateBranchNotification(branch: $this->branch, user: $this->user));
+    }
+}
