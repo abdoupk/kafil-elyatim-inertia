f0dd0620d9fb3ccf7419f131118e3bfab407dd36
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-21 07:38:38 +0100

    Structuring statistics

--- resources/js/types/statistics.ts
+++ resources/js/types/statistics.ts
@@ -2,18 +2,46 @@ export type FamiliesByZoneType = {
     labels: string[]
     data: number[]
 }
+
 export type FamiliesByBranchType = {
     labels: string[]
     data: number[]
 }
+
 export type FamiliesByOrphansCountType = {
     total_families: number[]
     orphans_count: number[]
 }
+
 export type FamiliesSponsorShipsType = {
     ramadan_basket_count: number
     zakat_count: number
     housing_assistance_count: number
     eid_al_adha_count: number
 }
+
 export type FamiliesGroupByDateType = number[]
+
+export type OrphansByFamilyStatusType = number[]
+
+export type OrphansByAcademicLevelType = number[]
+
+export type OrphansBySponsorshipType = number[]
+
+export type OrphansByGenderType = number[]
+
+export type OrphansByAgeType = number[]
+
+export type OrphansByZoneType = number[]
+
+export type OrphansByBranchType = number[]
+
+export type OrphansByPantsAndShirtSizeType = number[]
+
+export type OrphansByShoeSizeType = number[]
+
+export type OrphansByVocationalTrainingType = number[]
+
+export type OrphansByCreatedDateType = number[]
+
+export type OrphansGroupHealthStatusType = number[]
