3b75bf328d544bc0b7bee2a25c3e30b32a470237
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-23 17:46:58 +0100

    WIP Optimization

--- resources/js/Pages/Tenant/dashboard/schedules/TheExpandedCalendar.vue
+++ resources/js/Pages/Tenant/dashboard/schedules/TheExpandedCalendar.vue
@@ -1,11 +1,43 @@
 <script lang="ts" setup>
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
 
 const BaseExpandedVCalendar = defineAsyncComponent(
     () => import('@/Components/Base/VCalendar/BaseExpandedVCalendar.vue')
 )
+
+const date = new Date()
+
+const year = date.getFullYear()
+
+const month = date.getMonth()
+
+const attributes = ref([
+    {
+        key: 'today',
+        highlight: {
+            color: 'purple',
+            fillMode: 'solid',
+            contentClass: 'italic'
+        },
+        dates: new Date(year, month, 12)
+    },
+    {
+        highlight: {
+            color: 'purple',
+            fillMode: 'light'
+        },
+        dates: new Date(year, month, 13)
+    },
+    {
+        highlight: {
+            color: 'purple',
+            fillMode: 'outline'
+        },
+        dates: new Date(year, month, 14)
+    }
+])
 </script>
 
 <template>
-    <base-expanded-v-calendar></base-expanded-v-calendar>
+    <base-expanded-v-calendar :attributes="attributes"></base-expanded-v-calendar>
 </template>
