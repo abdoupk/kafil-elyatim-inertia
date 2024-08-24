dc33612372bd0f247fed7a758bf09b86bed077df
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-24 22:33:34 +0100

    families index page lazy loading

--- resources/js/Pages/Tenant/families/index/FamiliesIndexPage.vue
+++ resources/js/Pages/Tenant/families/index/FamiliesIndexPage.vue
@@ -3,21 +3,28 @@ import type { FamiliesIndexResource, IndexParams, PaginationData } from '@/types
 
 import { familiesFilters } from '@/constants/filters'
 import { Head, router } from '@inertiajs/vue3'
-import { reactive, ref } from 'vue'
+import { defineAsyncComponent, reactive, ref } from 'vue'
 
 import TheLayout from '@/Layouts/TheLayout.vue'
 
-import DataTable from '@/Pages/Tenant/families/index/DataTable.vue'
-
-import BaseButton from '@/Components/Base/button/BaseButton.vue'
-import TheNoResultsTable from '@/Components/Global/DataTable/TheNoResultsTable.vue'
-import TheTableFooter from '@/Components/Global/DataTable/TheTableFooter.vue'
-import TheTableHeader from '@/Components/Global/DataTable/TheTableHeader.vue'
-import DeleteModal from '@/Components/Global/DeleteModal.vue'
-import SuccessNotification from '@/Components/Global/SuccessNotification.vue'
+import TheContentLoader from '@/Components/Global/theContentLoader.vue'
 
 import { getDataForIndexPages, handleSort } from '@/utils/helper'
 import { n__ } from '@/utils/i18n'
+
+const DataTable = defineAsyncComponent(() => import('@/Pages/Tenant/families/index/DataTable.vue'))
+
+const BaseButton = defineAsyncComponent(() => import('@/Components/Base/button/BaseButton.vue'))
+
+const TheNoResultsTable = defineAsyncComponent(() => import('@/Components/Global/DataTable/TheNoResultsTable.vue'))
+
+const TheTableFooter = defineAsyncComponent(() => import('@/Components/Global/DataTable/TheTableFooter.vue'))
+
+const TheTableHeader = defineAsyncComponent(() => import('@/Components/Global/DataTable/TheTableHeader.vue'))
+
+const DeleteModal = defineAsyncComponent(() => import('@/Components/Global/DeleteModal.vue'))
+
+const SuccessNotification = defineAsyncComponent(() => import('@/Components/Global/SuccessNotification.vue'))
 
 defineOptions({
     layout: TheLayout
@@ -93,48 +100,60 @@ const showDeleteModal = (familyId: string) => {
 <template>
     <Head :title="$t('the_families')"></Head>
 
-    <the-table-header
-        :filters="familiesFilters"
-        :pagination-data="families"
-        :params="params"
-        :title="$t('list', { attribute: $t('the_families') })"
-        :url="route('tenant.families.index')"
-        entries="families"
-        export-pdf-url="tenant.families.export.pdf"
-        export-xlsx-url="tenant.families.export.xlsx"
-        exportable
-        filterable
-        searchable
-        @change-filters="params = $event"
-    >
-        <template #ExtraButtons>
-            <base-button
-                class="me-2 shadow-md"
-                variant="primary"
-                @click.prevent="router.get(route('tenant.families.create'))"
+    <suspense>
+        <div>
+            <the-table-header
+                :filters="familiesFilters"
+                :pagination-data="families"
+                :params="params"
+                :title="$t('list', { attribute: $t('the_families') })"
+                :url="route('tenant.families.index')"
+                entries="families"
+                export-pdf-url="tenant.families.export.pdf"
+                export-xlsx-url="tenant.families.export.xlsx"
+                exportable
+                filterable
+                searchable
+                @change-filters="params = $event"
             >
-                {{ n__('add new', 0, { attribute: $t('family') }) }}
-            </base-button>
+                <template #ExtraButtons>
+                    <base-button
+                        class="me-2 shadow-md"
+                        variant="primary"
+                        @click.prevent="router.get(route('tenant.families.create'))"
+                    >
+                        {{ n__('add new', 0, { attribute: $t('family') }) }}
+                    </base-button>
+                </template>
+            </the-table-header>
+
+            <template v-if="families.data.length > 0">
+                <data-table :families :params @showDeleteModal="showDeleteModal" @sort="sort"></data-table>
+
+                <the-table-footer
+                    :pagination-data="families"
+                    :params
+                    :url="route('tenant.families.index')"
+                ></the-table-footer>
+            </template>
+
+            <the-no-results-table v-else></the-no-results-table>
+
+            <delete-modal
+                :deleteProgress
+                :open="deleteModalStatus"
+                @close="closeDeleteModal"
+                @delete="deleteFamily"
+            ></delete-modal>
+
+            <success-notification
+                :open="showSuccessNotification"
+                :title="n__('successfully_trashed', 0, { attribute: $t('the_family') })"
+            ></success-notification>
+        </div>
+
+        <template #fallback>
+            <the-content-loader></the-content-loader>
         </template>
-    </the-table-header>
-
-    <template v-if="families.data.length > 0">
-        <data-table :families :params @showDeleteModal="showDeleteModal" @sort="sort"></data-table>
-
-        <the-table-footer :pagination-data="families" :params :url="route('tenant.families.index')"></the-table-footer>
-    </template>
-
-    <the-no-results-table v-else></the-no-results-table>
-
-    <delete-modal
-        :deleteProgress
-        :open="deleteModalStatus"
-        @close="closeDeleteModal"
-        @delete="deleteFamily"
-    ></delete-modal>
-
-    <success-notification
-        :open="showSuccessNotification"
-        :title="n__('successfully_trashed', 0, { attribute: $t('the_family') })"
-    ></success-notification>
+    </suspense>
 </template>
