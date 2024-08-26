4efca149121eda7eb4ee8a78df9a41a124c7f33c
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-26 07:48:59 +0100

    WIP optimization

--- resources/js/Pages/Tenant/dashboard/schedules/TheSchedules.vue
+++ resources/js/Pages/Tenant/dashboard/schedules/TheSchedules.vue
@@ -41,16 +41,20 @@ const attributes = computed(() => [
             <the-expanded-calendar :attributes class="px-1 py-2.5"></the-expanded-calendar>
 
             <div class="border-t border-slate-200/60 px-5 pb-5">
-                <div v-for="event in comingEvents" :key="event.id" class="mt-4 flex items-center first:mt-5">
-                    <div
-                        :style="{
-                            'background-color': event.color
-                        }"
-                        class="me-3 h-2 w-2 rounded-full"
-                    ></div>
-                    <span class="truncate">{{ event.title }}</span
-                    ><span class="ms-auto font-medium">{{ formatDateAndTimeShort(event.date) }}</span>
-                </div>
+                <template v-if="comingEvents.length">
+                    <div v-for="event in comingEvents" :key="event.id" class="mt-4 flex items-center first:mt-5">
+                        <div
+                            :style="{
+                                'background-color': event.color
+                            }"
+                            class="me-3 h-2 w-2 rounded-full"
+                        ></div>
+                        <span class="truncate">{{ event.title }}</span
+                        ><span class="ms-auto font-medium">{{ formatDateAndTimeShort(event.date) }}</span>
+                    </div>
+                </template>
+
+                <div v-else class="mt-5 text-center">no schedules</div>
             </div>
         </div>
     </suspense>
