b1f46bda70e85555f3a93fb75cf0896d55511af8
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-08-29 01:43:05 +0100

    WIP

--- resources/views/pdf/financial-transactions.blade.php
+++ resources/views/pdf/financial-transactions.blade.php
@@ -1,73 +1,65 @@
 <x-table>
     <x-slot name="thead">
-        <x-th class=" py-0.5 ">
+        <x-th>
             <span> #</span>
         </x-th>
 
-        <x-th class=" py-0.5 ">
+        <x-th>
             {{ __('receiving_member') }}
         </x-th>
 
-        <x-th class=" py-0.5 ">
+        <x-th>
             {{ __('the_amount') }}
         </x-th>
 
-        <x-th class=" py-0.5 ">
+        <x-th>
             {{ __('validation.attributes.specification') }}
         </x-th>
 
-        <x-th class=" py-0.5 ">
+        <x-th>
+            {{ __('the_type') }}
+        </x-th>
+
+        <x-th>
             {{ __('the date') }}
-        </x-th>
-
-        <x-th class=" py-0.5 ">
-            {{ __('validation.attributes.created_at') }}
-        </x-th>
-
-        <x-th class=" py-0.5 ">
-            {{ __('created_by') }}
         </x-th>
     </x-slot>
 
     <x-slot name="tbody">
         @foreach ($transactions as $transaction)
             <tr>
-                <x-td class=" py-0.5  text-center">
+                <x-td class="   text-center">
                     {{ $loop->iteration }}
                 </x-td>
 
-                <x-td class=" py-0.5  text-center">
+                <x-td class="   text-center">
                     {{ $transaction->receiver?->getName() }}
                 </x-td>
 
-                <x-td class=" py-0.5  text-center">
-                    {{ formatCurrency($transaction->amount) }}
+                <x-td class="   text-center">
+                    {{ formatCurrency(abs($transaction->amount)) }}
                 </x-td>
 
-                <x-td class=" py-0.5  text-center">
+                <x-td class="   text-center">
                     {{ __($transaction->specification) }}
                 </x-td>
 
-                <x-td class=" py-0.5  text-center">
-                    {{ $transaction->date->translatedFormat('j F Y H:i A') }}
+                <x-td class="text-center">
+                    {{ $transaction->amount  >0 ? __('income') : __('expense')}}
                 </x-td>
 
-                <x-td class=" py-0.5  text-center">
-                    {{ $transaction->created_at->translatedFormat('j F Y H:i A') }}
-                </x-td>
-
-                <x-td class=" py-0.5  text-center">
-                    {{ $transaction->creator->getName() }}
+                <x-td class="   text-center">
+                    {{ $transaction->date->translatedFormat('j F Y') }}
                 </x-td>
             </tr>
         @endforeach
 
         <tr>
-            <x-td class=" py-0.5  text-center font-semibold" colspan="2">
+            <x-td class="   text-center font-semibold" colspan="2">
                 {{ __('the_total_amount') }}
             </x-td>
 
-            <x-td class=" py-0.5  text-center">
+            <x-td class="   text-center">
                 {{ formatCurrency($transactions->sum('amount')) }}
             </x-td>
         </tr>
