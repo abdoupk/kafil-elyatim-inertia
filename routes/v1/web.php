e3a055c10d69c304587004873b84d88e0e00900e
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-09-04 12:07:11 +0100

    FIX

--- routes/v1/web.php
+++ routes/v1/web.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\V1\Landing\HomePageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomePageController::class);
+// for now, we redirect to register before create landing page.
+Route::get('/', fn () => redirect('/register'));
