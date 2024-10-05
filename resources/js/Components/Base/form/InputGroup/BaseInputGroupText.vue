1344dc910a3fc121d6ee2e216d0a0f20a6e63593
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-10-05 17:24:05 +0100

    fix issues

--- resources/js/Components/Base/form/InputGroup/BaseInputGroupText.vue
+++ resources/js/Components/Base/form/InputGroup/BaseInputGroupText.vue
@@ -16,7 +16,7 @@ const inputGroup = inject<ProvideInputGroup>('inputGroup')
 
 const computedClass = computed(() =>
     twMerge([
-        'py-2 px-3 bg-slate-100 border shadow-sm border-slate-200 text-slate-600 dark:bg-darkmode-900/20 dark:border-darkmode-900/20 dark:text-slate-400',
+        'py-2 px-3 whitespace-nowrap bg-slate-100 border shadow-sm border-slate-200 text-slate-600 dark:bg-darkmode-900/20 dark:border-darkmode-900/20 dark:text-slate-400',
         inputGroup && 'rounded-none [&:not(:first-child)]:border-s-transparent first:rounded-s last:rounded-e',
         typeof attrs.class === 'string' && attrs.class
     ])
